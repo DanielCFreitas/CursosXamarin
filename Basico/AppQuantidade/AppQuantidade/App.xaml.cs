31d736616cbaa8acba746e13e4af617b9a4f3209
Daniel Freitas <daniel.freitas@treevia.com.br>
2022-09-03 12:31:36 -0300

    Base primeira aplicacao com maui

---
+++ Basico/AppQuantidade/AppQuantidade/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AppQuantidade;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Basico/AppQuantidade/AppQuantidade/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppQuantidade;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
