4ac1009bb8f304cdebc2e6866e416b74c451120a
Daniel Freitas <daniel.freitas@treevia.com.br>
2022-09-07 10:39:09 -0300

    Usando paginas do tipo carousel

--- Basico/AppGallery/AppGallery/AppGallery/App.xaml.cs
+++ Basico/AppGallery/AppGallery/AppGallery/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using AppGallery.XamarinForms.Paginas.PaginaDeNavegacao;
+﻿using AppGallery.XamarinForms.Paginas.PaginaDeCarrossel;
 using Xamarin.Forms;
 
 namespace AppGallery
@@ -9,8 +9,29 @@ namespace AppGallery
         {
             InitializeComponent();
 
-            //var pagina = new NavigationPage(new Conteudo01());
-            MainPage = new XamarinForms.Paginas.Modal.Conteudo01();
+            /**
+             * NavigationPage
+             * */
+            // MainPage = new NavigationPage(new Conteudo01());  
+            
+
+            /**
+             *  Modal
+             * */
+            // MainPage = new XamarinForms.Paginas.Modal.Conteudo01();
+
+
+            /**
+             *  Carousel
+             * */
+            //var carousel = new CarouselPage();
+            //carousel.Children.Add(new Conteudo01());
+            //carousel.Children.Add(new Conteudo02());
+            //carousel.Children.Add(new Conteudo03());
+            //carousel.CurrentPage = carousel.Children[1];
+            MainPage = new XamarinForms.Paginas.PaginaDeCarrossel.Carousel();
+
+
         }
 
         protected override void OnStart()
