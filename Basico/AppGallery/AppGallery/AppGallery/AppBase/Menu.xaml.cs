33e6aa9ec47870ce1fca983534bea11274996e96
Daniel Freitas <daniel.freitas@treevia.com.br>
2022-09-24 09:48:12 -0300

    Flexlayout

--- Basico/AppGallery/AppGallery/AppGallery/AppBase/Menu.xaml.cs
+++ Basico/AppGallery/AppGallery/AppGallery/AppBase/Menu.xaml.cs
@@ -73,5 +73,11 @@ namespace AppGallery.AppBase
             ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Leiaouts.RelativoLeiaout.Relativo();
             ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
         }
+
+        private void AbrirFlexLayout(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Leiaouts.FlexivelLeiaout.Flexivel();
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
     }
 }
